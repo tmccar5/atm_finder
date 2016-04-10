cf909e13a4b0a7df81b95d8813b93102e298dec0
tmccar5 <tommccarthyguitar@gmail.comssh-keygen -t rsa -C tommccarthyguitar@gmail.comcat>
2016-04-10 18:46:25 -0500

    blue marker for user starting location entered

--- app/controllers/atms_controller.rb
+++ app/controllers/atms_controller.rb
@@ -1,9 +1,5 @@
 class AtmsController < ApplicationController
 	def index
-		# response = Atm.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=39.18619,-76.854895&keyword=atm&types=atm&rankby=distance&key=AIzaSyAny_eoq9b1UGL7Ow20vDTet9yIBb60BwA")
-		# @test = response.parsed_response
-		# @name = test["results"][0]["name"]
-		# render json: @test
 	end
 
 	def new
--- app/models/atm.rb
+++ app/models/atm.rb
@@ -2,11 +2,4 @@ class Atm < ActiveRecord::Base
 	geocoded_by :street_address
 	after_validation :geocode
 	include HTTParty
-	
-
-	# response = HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=39.18619,-76.854895&keyword=atm&types=atm&rankby=distance&key=AIzaSyAny_eoq9b1UGL7Ow20vDTet9yIBb60BwA")
-
-
-	# yes = response
-
 end
